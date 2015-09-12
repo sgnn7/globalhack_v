8405c70443617962cd8c864932dacab79e76ebd6
Auto Commiter <autocommiter@gh.com>
2015-09-12 18:32:11 -0500

    Autocommit - 150912-1832

--- public_html/application/views/includes/footer.php
+++ public_html/application/views/includes/footer.php
@@ -11,13 +11,13 @@
                         <h3>Follow us on Social Media</h3>
                         <ul class="list-inline">
                             <li>
-                                <a href="https://www.facebook.com/" class="btn-social btn-outline" alt="facebook"><i class="fa fa-fw fa-facebook"></i></a>
+                                <a href="https://www.facebook.com/" class="btn-social btn-outline" alt="facebook"><i class="fa fa-fw fa-facebook"></i>Facebook</a>
                             </li>
                             <li>
-                                <a href="https://accounts.google.com/ServiceLogin?service=oz&passive=1209600&continue=https://plus.google.com/?gpsrc%3Dgplp0#identifier" class="btn-social btn-outline" alt="google plus"><i class="fa fa-fw fa-google-plus"></i></a>
+                                <a href="https://accounts.google.com/ServiceLogin?service=oz&passive=1209600&continue=https://plus.google.com/?gpsrc%3Dgplp0#identifier" class="btn-social btn-outline" alt="google plus"><i class="fa fa-fw fa-google-plus">Google Plus</i></a>
                             </li>
                             <li>
-                                <a href="https://twitter.com/" class="btn-social btn-outline" alt="twitter"><i class="fa fa-fw fa-twitter"></i></a>
+                                <a href="https://twitter.com/" class="btn-social btn-outline" alt="twitter"><i class="fa fa-fw fa-twitter"></i>Twitter</a>
                             </li>
                         </ul>
                     </div>
