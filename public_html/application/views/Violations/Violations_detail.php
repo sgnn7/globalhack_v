a01b6c0e2fc32782ab22a90086f984bc7180dc5b
phawke <paul.hawke@asynchrony.com>
2015-09-12 11:16:50 -0500

    added detail screen. -P

--- public_html/application/views/Violations/Violations_detail.php
+++ public_html/application/views/Violations/Violations_detail.php
@@ -1,44 +1,32 @@
 <div class='container' style="padding-top:170px">
-    <div class="panel panel-default">
-        <h1>Citation Details</h1>
-        <h3><span>Citation date:</span> <span id="citation_date"><?php echo $Violations[0]->citation_date; ?></span></h3>
-    </div><br>
-    <div class="panel">
-        <span>Name:</span> <span id="name"><?php echo $Violations[0]->first_name.' '.$Violations[0]->last_name;  ?></span><br>
+    <div id="citation_header">
+        <h1>Citation Details (#<?php echo $Violations[0]->citation_number; ?>)</h1>
+
+        <h3><span>Citation date:</span> <span id="citation_date"><?php echo $Violations[0]->citation_date; ?></span>
+        </h3>
+    </div>
+
+    <div id="defendant">
+        <h3>Defendant</h3>
+        <span
+            id="name"><?php echo $Violations[0]->first_name . ' ' . $Violations[0]->last_name; ?></span><br>
+        <?php if (strlen($Violations[0]->drivers_license_number) > 0) { ?>
+            <span id="drivers_license_label">Driver's license #:</span> <span
+                id="drivers_license"><?php echo $Violations[0]->drivers_license_number; ?></span><br>
+        <?php } ?>
         <div class="defendant_address">
             <span id="address_line"><?php echo $Violations[0]->defendant_address; ?></span><br>
-<!--            <span id="address_line">--><?php //echo $Violations[0]->defendant_city.', '.$Violations[0]->defendant_state.', '; ?><!--</span><br>-->
+            <span
+                id="address_city_state"><?php echo $Violations[0]->defendant_city . ', ' . $Violations[0]->defendant_state; ?></span><br>
         </div>
-<!--        <div class="court">-->
-<!--            <span>Court date:</span> <span id="court_date">--><?php //echo $Violations[0]->court_date; ?><!--</span>-->
-<!--            <span id="address_line">--><?php //echo $Violations[0]->court_address.', '.$Violations[0]->court_location.', '; ?><!--</span><br>-->
-<!--        </div>-->
     </div>
-    <div class='panel panel-default'>
-        <pre><?php var_dump($Violations)  ?>
-        <hr>
-        <table class='table table-striped table-hover table-condensed'>
-            <thead>
-            <tr class='row-fluid small'>
-                <th>
-                    <center>Date</center>
-                </th>
-                <th>
-                    <center>Violation Description</center>
-                </th>
-                <th>
-                    <center>Citation #</center>
-                </th>
-                <th>
-                    <center>Municipality</center>
-                </th>
-                <th>
-                    <center>Amount Owed</center>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
+    <br>
+
+    <div class="court">
+        <h3>Court</h3>
+        <span id="court_date_label">Court date:</span> <span
+            id="court_date"><?php echo $Violations[0]->court_date; ?></span><br>
+        <span
+            id="address_line"><?php echo $Violations[0]->court_address . ', ' . $Violations[0]->court_location; ?></span><br>
     </div>
 </div>
