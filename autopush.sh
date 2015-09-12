b9633d0d80e9675fa6766c5d74b2dc24fc6db9be
Auto Commiter <autocommiter@gh.com>
2015-09-12 06:08:27 -0500

    Autocommit - 150912-0608

--- autopush.sh
+++ autopush.sh
@@ -2,6 +2,7 @@
 
 while [ true ]; do
   echo "Grabbing...";
+  git pull
   rsync -avz --delete ~/public_html . && git add . && git commit -m "Autocommit - $(date +%y%m%d-%H%M)" && git push origin HEAD;
   echo "Sleeping...";
   sleep 180;
