bd1d47ff32ba48845ded45ccd5cd46ab70039458
Auto Commiter <autocommiter@gh.com>
2015-09-12 16:52:26 -0500

    Autocommit - 150912-1652

--- public_html/application/views/Volunteer/home.php
+++ public_html/application/views/Volunteer/home.php
@@ -1,12 +1,13 @@
 <div class='container fuelux' style="padding-top:170px">
     <script>
         $(function () {
+            alert("foo!!!!")
             $('#exampleModal').on('show.bs.modal', function (event) {
                 var button = $(event.relatedTarget) // Button that triggered the modal
                 var recipient = button.data('whatever') // Extract info from data-* attributes
                 var modal = $(this);
                 modal.find('.modal-title').text('New message to ' + recipient)
-                modal.find('.modal-body').text(recipient)
+                modal.find('.modal-body').innerHTML('<p>'+recipient+'</p>')
             });
         });
     </script>
