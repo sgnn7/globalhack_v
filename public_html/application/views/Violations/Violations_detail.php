360ec02717be26c2f9977ca4cab9a64b2dd87818
Auto Commiter <autocommiter@gh.com>
2015-09-12 11:10:59 -0500

    Autocommit - 150912-1110

--- public_html/application/views/Violations/Violations_detail.php
+++ public_html/application/views/Violations/Violations_detail.php
@@ -5,6 +5,9 @@
     </div><br>
     <div class="panel">
         <span>Name:</span> <span id="name"><?php echo $Violations[0]->first_name.' '.$Violations[0]->last_name;  ?></span><br>
+        <?php if (strlen($Violations[0]->drivers_license_number) > 0) { ?>
+            <span>Driver's license #:</span> <span id="license"><?php echo $Violations[0]->drivers_license_number; ?></span><br>
+        <?php } ?>
         <div class="defendant_address">
             <span id="address_line"><?php echo $Violations[0]->defendant_address; ?></span><br>
             <span id="address_line"><?php echo $Violations[0]->defendant_city.', '.$Violations[0]->defendant_state; ?></span><br>
@@ -14,32 +17,5 @@
             <span>Court date:</span> <span id="court_date"><?php echo $Violations[0]->court_date; ?></span><br>
             <span id="address_line"><?php echo $Violations[0]->court_address.', '.$Violations[0]->court_location; ?></span><br>
         </div>
-    </div>
-    <div class='panel panel-default'>
-        <pre><?php var_dump($Violations)  ?>
-        <hr>
-        <table class='table table-striped table-hover table-condensed'>
-            <thead>
-            <tr class='row-fluid small'>
-                <th>
-                    <center>Date</center>
-                </th>
-                <th>
-                    <center>Violation Description</center>
-                </th>
-                <th>
-                    <center>Citation #</center>
-                </th>
-                <th>
-                    <center>Municipality</center>
-                </th>
-                <th>
-                    <center>Amount Owed</center>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
     </div>
 </div>
