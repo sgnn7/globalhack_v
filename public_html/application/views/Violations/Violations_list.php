a53a2b1763dde755e3971843c087c74cb1378365
Auto Commiter <autocommiter@gh.com>
2015-09-12 04:57:51 -0500

    Autocommit - 150912-0457

--- public_html/application/views/Violations/Violations_list.php
+++ public_html/application/views/Violations/Violations_list.php
@@ -1,23 +1,24 @@
 <div class='container' style="padding-top:170px">
   <div class='panel panel-default'>
     <table class='table table-striped table-hover table-condensed'>
-	<tr class='row-fluid small'>
-	<th>
-	<center>Date</center>
-	</th>
-	<th>
-	<center>Violation Description</center>
-	</th>
-	<th>
-	<center>Citation #</center>
-	</th>
-	<th>
-	<center>Municipality</center>
-	</th>
-	<th>
-	<center>Amount Owed</center>
-	</th>
-	</tr>
+		<tr class='row-fluid small'>
+			<th>
+				<center>Date</center>
+			</th>
+			<th>
+				<center>Violation Description</center>
+			</th>
+			<th>
+				<center>Citation #</center>
+			</th>
+			<th>
+				<center>Municipality</center>
+			</th>
+			<th>
+				<center>Amount Owed</center>
+			</th>
+		</tr>
+	</
 	<tbody>
 		<?php foreach($Violations as $violation){?>
 		<tr class='row-fluid rowlink' data-target='/link/to/violation' title='Violation'>
