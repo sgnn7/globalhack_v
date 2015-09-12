d7051daa5034ada62013bdb083f8dcd4a93b0c81
Auto Commiter <autocommiter@gh.com>
2015-09-12 04:42:46 -0500

    Autocommit - 150912-0442

--- public_html/application/views/Violations/Violations_list.php
+++ public_html/application/views/Violations/Violations_list.php
@@ -21,34 +21,35 @@
 	<tbody>
 		<?php foreach($Violations as $violation){?>
 		<tr class='row-fluid rowlink' data-target='/link/to/violation' title='Violation'>
-	<td class='rowlink-skip'>
-	<center class='small-text'>
-	<nobr><?=$violation->status_date;?></nobr>
-	</center>
-	</td>
-	<td class='rowlink-skip'>
-	<center class='small-text'>
-	<nobr><?=$violation->violation_description;?></nobr>
-	</center>
-	</td>
-	<td class='clickable'>
-	<center class='small-text'><?=$violation->citation_number;?></center>
-	</td>
-	<td class='clickable'>
-	<center class='small-text'>All</center>
-	</td>
-	<td class='clickable'>
-	<center class='small-text'><?=($violation->fine_amount + $violation->court_cost);?></center>
-	</td>
-	<td class='rowlink-skip'>
-	<center>
-	<nobr>
-	<a class='btn btn-xs btn-info link-button' href='somelink' title='Get stuff'>
-        <i class="fa fa-link"></i>
-	</a>
-	</nobr>
-	</center>
-	</td>
+			<td class='rowlink-skip'>
+				<center class='small-text'>
+					<nobr><?=$violation->status_date;?></nobr>
+				</center>
+			</td>
+			<td class='rowlink-skip'>
+				<center class='small-text'>
+					<nobr><?=$violation->violation_description;?></nobr>
+				</center>
+			</td>
+			<td class='clickable'>
+				<center class='small-text'><?=$violation->citation_number;?></center>
+			</td>
+			<td class='clickable'>
+				<center class='small-text'>All</center>
+			</td>
+			<? $fine_amount = $violation->fine_amoun
+			<td class='clickable'>
+				<center class='small-text'><?=($violation->fine_amount + $violation->court_cost);?></center>
+			</td>
+			<td class='rowlink-skip'>
+				<center>
+					<nobr>
+						<a class='btn btn-xs btn-info link-button' href='somelink' title='Get stuff'>
+        				<i class="fa fa-link"></i>
+						</a>
+					</nobr>
+				</center>
+			</td>
 			<? } ?>
 	</tbody>
     </table>
