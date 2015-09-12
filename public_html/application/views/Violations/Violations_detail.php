47352f3f4624f3a1f3fe23a81039f96208314f80
phawke <paul.hawke@asynchrony.com>
2015-09-12 15:02:38 -0500

    Edited detail screen. -P

--- public_html/application/views/Violations/Violations_detail.php
+++ public_html/application/views/Violations/Violations_detail.php
@@ -10,8 +10,11 @@
         <h3>Defendant</h3>
         <span
             id="name"><?php echo $Citations[0]->first_name . ' ' . $Citations[0]->last_name; ?></span><br>
-        <?php if (strcmp("TRUE", $Violations[0]->warrant_status) == 0) {?>
-            <span id="warrant_status" style="color: #ff0000; font-weight: bold"><?php echo $Violations[0]->status; ?></span> <span id="warrant_info"><a href="http://www.dmv.org/articles/handling-a-warrant-for-your-arrest/">(more info)</a></span><br>
+        <?php if (strcmp("TRUE", $Violations[0]->warrant_status) == 0) { ?>
+            <span id="warrant_status"
+                  style="color: #ff0000; font-weight: bold"><?php echo $Violations[0]->status; ?></span> <span
+                id="warrant_info"><a href="http://www.dmv.org/articles/handling-a-warrant-for-your-arrest/">(more
+                    info)</a></span><br>
         <?php } ?>
         <?php if (strlen($Citations[0]->drivers_license_number) > 0) { ?>
             <span id="drivers_license_label">Driver's license #:</span> <span
@@ -26,24 +29,27 @@
     <br>
 
     <div class="court">
-        <h3>Court</h3>
-        <div class="court_address">
-            <span id="municipality"><?php echo $Courts[0]->Municipality; ?></span> <span id="court_map"><a href="https://maps.google.com/maps?q=<?php echo $Courts[0]->Y.','.$Courts[0]->X.'&z=17'; ?>">(map)</a></span><br>
-            <span id="address_line"><?php echo $Courts[0]->Address; ?></span><br>
+        <h3>Court Date <?php echo $Citations[0]->court_date; ?></h3>
+        <?php if (isset($Courts) && count($Courts) > 0) { ?>
+            <div class="court_address">
+                <span id="municipality"><?php echo $Courts[0]->Municipality; ?></span> <span id="court_map"><a
+                        href="https://maps.google.com/maps?q=<?php echo $Courts[0]->Y . ',' . $Courts[0]->X . '&z=17'; ?>">(map)</a></span><br>
+                <span id="address_line"><?php echo $Courts[0]->Address; ?></span><br>
             <span
                 id="address_city_state"><?php echo $Courts[0]->City . ', ' . $Courts[0]->State . ', ' . $Courts[0]->Zip_Code; ?></span><br>
-        </div>
-        <span id="violation_description"><?php $Violations[0]->violation_description; ?></span><br>
-        <span id="violation_fine_label">Fine:</span>
-        <span id="violation_fine"><?php echo $Violations[0]->fine_amount; ?></span><br>
-        <span id="violation_cost_label">Court cost:</span>
-        <span id="violation_cost"><?php echo $Violations[0]->court_cost; ?></span><br>
-        <span id="total_owed_label">Total owed:</span>
+            </div><br>
+        <?php } ?>
+        <span id="violation_description"><?php echo $Violations[0]->violation_description; ?></span><br>
+        <?php if (isset($Violations[0]->fine_amount)) { ?>
+            <span id="violation_fine_label">Fine:</span>
+            <span id="violation_fine"><?php echo $Violations[0]->fine_amount; ?></span><br>
+            <span id="violation_cost_label">Court cost:</span>
+            <span id="violation_cost"><?php echo $Violations[0]->court_cost; ?></span><br>
+            <span id="total_owed_label">Total owed:</span>
         <span id="total_owed_label"><?php
             $total = floatval($Violations[0]->fine_amount) + floatval($Violations[0]->court_cost);
             echo number_format($total, 2);
             ?></span><br><br>
-        <span id="court_date_label">Court date:</span> <span
-            id="court_date"><?php echo $Citations[0]->court_date; ?></span><br>
+        <?php } ?>
     </div>
 </div>
