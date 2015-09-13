e67a46381d52c19b7c775366530bbab5aaa0787c
Auto Commiter <autocommiter@gh.com>
2015-09-13 02:49:26 -0500

    Autocommit - 150913-0249

--- public_html/application/views/Volunteer/home.php
+++ public_html/application/views/Volunteer/home.php
@@ -132,14 +132,14 @@
 </div>
 	
 <script>
-	$(function () {
 		$('#myModal').bind('show.bs.modal', function (event) {
 			alert("fun!");
 			var button = $(event.relatedTarget) // Button that triggered the modal
 			var recipient = button.data('whatever') // Extract info from data-* attributes
-			var modal = $(this);
+			// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+			// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+			var modal = $(this)
 			modal.find('.modal-title').text('New message to ' + recipient)
-			modal.find('.modal-body').innerHTML('<p>'+recipient+'</p>')
+			modal.find('.modal-body input').val(recipient)
 		});
-	});
 </script>
