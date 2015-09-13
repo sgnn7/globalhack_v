b4b8451ccf438ff34e1afbc127c736b2945a6f64
Auto Commiter <autocommiter@gh.com>
2015-09-13 03:55:57 -0500

    Autocommit - 150913-0355

--- public_html/application/views/includes/header.php
+++ public_html/application/views/includes/header.php
@@ -32,15 +32,16 @@
 <!-- 	  <script src="https://raw.githubusercontent.com/ExactTarget/fuelux/3.11.0/js/wizard.js"></script> -->
 	  <script src="https://www.fuelcdn.com/fuelux/3.11.0/js/fuelux.min.js"></script>
 
-	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+<!-- 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script> -->
 	  
 <!-- 	  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script> -->
 
 
 
-		<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		
 
 		<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	  <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 
 <!-- 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
