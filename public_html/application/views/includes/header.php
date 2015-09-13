f9c12219911124619925da36e6aa9decd94db8a4
Auto Commiter <autocommiter@gh.com>
2015-09-13 05:14:34 -0500

    Autocommit - 150913-0514

--- public_html/application/views/includes/header.php
+++ public_html/application/views/includes/header.php
@@ -3,8 +3,23 @@
 	<head>
 		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 		<meta charset="utf-8" />
-		<title>Globalhack App</title>
+		<title>Pashle</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		
+		
 		<link rel="apple-touch-icon" href="assets/img/ico/60.png" alt="something">
 		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/ico/76.png">
 		<link rel="apple-touch-icon" sizes="120x120" href="assets/img/ico/120.png">
@@ -13,8 +28,8 @@
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-touch-fullscreen" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="default">
-		<meta content="" name="description" />
-		<meta content="" name="author" />
+		<meta content="" name="Pashle app for checking ticket and warrant information." />
+		<meta content="" name="globalhack-AwesomeSauce" />
 
 		<!--Style Sheets -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
