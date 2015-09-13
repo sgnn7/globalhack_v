d4c1fa30cae9eefe734a273df1562b7c4104a189
Auto Commiter <autocommiter@gh.com>
2015-09-13 08:40:11 -0500

    Autocommit - 150913-0840

--- public_html/application/views/Landing/verify.php
+++ public_html/application/views/Landing/verify.php
@@ -9,7 +9,7 @@
 </section>
 
 <section id="search" style="margin-top:-105px">
-	<form role="form" name="search_form" class="form" id="search_form">
+	<form role="form" name="search_form" class="form" id="search_form" action="">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6">
@@ -19,7 +19,7 @@
 					<div class="input-group">
 						<input type="text" name="SSN" class="form-control input-lg" placeholder="Please enter last 4 digits of SSN"/>					
 						<span class="input-group-btn">
-							<input type="button" id="click_search" class="btn btn-lg btn-complete btn-default" value="SEARCH"/>
+							<input type="submit" class="btn btn-lg btn-complete btn-default" value="SEARCH"/>
 						</span>
 					</div>
 				</div>
