5988fad204ac82d092dbe14ad1cd07ad0ea01cb4
Auto Commiter <autocommiter@gh.com>
2015-09-13 02:28:16 -0500

    Autocommit - 150913-0228

--- public_html/application/views/Information/information_page.php
+++ public_html/application/views/Information/information_page.php
@@ -31,76 +31,67 @@
 	
 
 <div class="container">
-	  
-		<div class="row">
-	    
+	<div class="row">	    
 		<div class="col-md-6" style="text-align:center;">
-	      
 		</div>
-
-		</div>
+	</div>
 	  
-<div id="accordion"  collapsible="false" active="false">
-	    
+	<div id="accordion"  collapsible="false" active="false">
 
-	<h3>I received a speeding ticket. What's next?</h3>
 
-	<div><p>Please pay requested amount on the citation document.</p></div>	    	    
+		<h3>I received a speeding ticket. What's next?</h3>
 
-	<h3>I lost my citation documents. How can I look up information relevant to my speeding ticket?</h3>
-	    
-	<div><p>Please use the search box on our website.</p>    
-	</div> 
+		<div><p>Please pay requested amount on the citation document.</p></div>	    	    
 
-	<h3>I received a court date. What must I do?</h3>
-	    
-	<div><p>Please appear in court at requested time, or contact court for extension.
-</p>
-</div>
-	    
+		<h3>I lost my citation documents. How can I look up information relevant to my speeding ticket?</h3>
 
-	<h3>What should I do before I go to court?</h3>
-	    
-	<div>
-<p>Contact a lawyer who can represent your rights.</p></div>
-	    
+		<div><p>Please use the search box on our website.</p>    
+		</div> 
 
-	<h3>What happens in court?</h3>
-	    
-	<div>
-<p>
-A judge will hear your case and will make judgement according to all available information.</p></div>
+		<h3>I received a court date. What must I do?</h3>
 
-	<h3>What are my rights?</h3>
-	    
-	<div>
-<p>
-You have the right to get legal representation in court.</p></div>
+		<div><p>Please appear in court at requested time, or contact court for extension.
+	</p>
+	</div>
 
-	<h3>How can I get a lawyer?</h3>
-	    
-	<div>
-<p>
-Please work with the state court to get recommendations, or visit the website of the Missouri BAR Association.</p></div>
 
-	<h3>What will happen if I ignore the ticket?</h3>
-	    
-	<div>
-<p>
-Your case will be brought to the court system and will be tried by an assigned judge.</p></div>
+		<h3>What should I do before I go to court?</h3>
 
-</div>
-</div>
-      
+		<div>
+	<p>Contact a lawyer who can represent your rights.</p></div>
 
-</form>
-    
-</section>
 
-    
-<div id="result">
-      
-    
+		<h3>What happens in court?</h3>
+
+		<div>
+	<p>
+	A judge will hear your case and will make judgement according to all available information.</p></div>
+
+		<h3>What are my rights?</h3>
+
+		<div>
+	<p>
+	You have the right to get legal representation in court.</p></div>
+
+		<h3>How can I get a lawyer?</h3>
+
+		<div>
+	<p>
+	Please work with the state court to get recommendations, or visit the website of the Missouri BAR Association.</p></div>
+
+		<h3>What will happen if I ignore the ticket?</h3>
+
+		<div>
+	<p>
+	Your case will be brought to the court system and will be tried by an assigned judge.</p></div>
+
+	</div>
+	</div>
+
+
+	</form>
+
+	<div id="result">    
 </div>
     
 
