f620157ffb21f920630416e2fe4e28ce266d850e
Auto Commiter <autocommiter@gh.com>
2015-09-12 20:41:35 -0500

    Autocommit - 150912-2041

--- public_html/application/views/includes/footer2.php
+++ public_html/application/views/includes/footer2.php
@@ -41,7 +41,7 @@
 
     <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
     <div class="scroll-top page-scroll visible-xs visible-sm">
-        <a href="#top">Go to top</a>
+        <a class="btn btn-primary" href="#page-top">
             <i class="fa fa-chevron-up"></i>
         </a>
     </div>
