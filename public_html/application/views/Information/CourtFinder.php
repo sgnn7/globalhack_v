7801a0276f7e18d21bde65e36b8a732a8dd90347
Auto Commiter <autocommiter@gh.com>
2015-09-13 11:02:24 -0500

    Autocommit - 150913-1102

--- public_html/application/views/Information/CourtFinder.php
+++ public_html/application/views/Information/CourtFinder.php
@@ -1,14 +1,17 @@
-<div class="container">
+<div class="container" style="padding-top:120px; padding-bottom:30px;">
 	<div class="row">
 		<h3>
-			Court Finder
+			Court Finder By Zip Code
 		</h3>
 	</div>
 	<div class="container">
 		<div class="row">
 			<form name="court_finder" action="<?=base_url();?>Information/search_court" method="post">
 			<div class="col-md-4">
-				<input type="search"
+				<input type="text" name="keyword" class="form-control" Placeholder="Enter Zip Code">
+			</div>
+			<div class="col-md-2">
+				<input type="submit" value="FIND Court" class="btn btn-primary">
 				</div>
 			</form>
 		</div>
